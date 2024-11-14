45839c779f46e82d4458d18603dcf176e3b2e069
samiclaros <cla24007@byui.edu>
2024-11-13 21:15:46 -0400

    Fighting with burguer button

---
+++ chamber/scripts/lastModified.js
@@ -0,0 +1,11 @@
+const lastModified = document.querySelector("#lastModified");
+const lastModifiedDate = new Date(document.lastModified);
+
+lastModified.innerHTML = `Last modified: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+	dateStyle: "short",
+    timeStyle: "medium"
+	}
+).format(lastModifiedDate)}</span>`;
+
