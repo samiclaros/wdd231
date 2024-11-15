9e991ff7410af3691583459075690cc0828e3918
samiclaros <cla24007@byui.edu>
2024-11-14 20:44:58 -0400

    Finished with all I was trying to fix

--- chamber/scripts/companiesCards.js
+++ chamber/scripts/companiesCards.js
@@ -5,7 +5,6 @@ async function getMembersData(url) {
     const response = await fetch(url);  
     const data = await response.json(); 
     displayMembers(data.members); 
-    console.error('Error al obtener los datos:', error);
 }
 
 const displayMembers = (members) => {
--- chamber/scripts/currentPage.js
+++ chamber/scripts/currentPage.js
@@ -4,9 +4,11 @@ const setActiveClass = (link) => {
     navLinks.forEach(nav => nav.classList.remove('active')); 
     link.classList.add('active');};
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.forEach(nav => nav.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
+    navLinks.forEach(link => { 
+        if (link.getAttribute('href') === window.location.pathname) { 
+            setActiveClass(link); 
+        }
+            link.addEventListener('click', (event) => { 
+                setActiveClass(event.currentTarget); 
+            }); 
+        });
