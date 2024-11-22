fc9c1c66eca67c6e94746fd9b30f7e02935b0a48
samiclaros <cla24007@byui.edu>
2024-11-21 20:24:51 -0400

    Forecast done, I need to add the cards of the members randomly below the menu and maybe fix the css

--- chamber/scripts/openWeather.js
+++ chamber/scripts/openWeather.js
@@ -1,6 +1,7 @@
 const currentTemp = document.querySelector("#temperature");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("#description");
+
 const maxTemperature = document.querySelector("#maxTemperature");
 const minTemperature = document.querySelector("#minTemperature");
 const humidity = document.querySelector("#humidity");
@@ -8,6 +9,7 @@ const sunrise = document.querySelector("#sunrise");
 const sunset = document.querySelector("#sunset");
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-17.42&lon=-66.17&appid=e7735ca7ff6b34ba2d5b96688c48601c&units=metric';
+const url2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=-17.42&lon=-66.17&appid=e7735ca7ff6b34ba2d5b96688c48601c&units=metric';
 
 async function apiFetch() {
     try{
@@ -20,11 +22,24 @@ async function apiFetch() {
             throw Error(await response.text());
         }
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
-apiFetch(url);
+async function apiFetch2(){
+    try {
+        const response = await fetch(url2);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayData2(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 function displayData(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;C`;
@@ -32,7 +47,7 @@ function displayData(data) {
     const desc = data.weather[0].description;
 
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('alt', 'Icon of the Actual Weather');
     weatherIcon.setAttribute('class', 'iconWeather');
     weatherIcon.setAttribute('width', 100);
     weatherIcon.setAttribute('height', 100);
@@ -44,9 +59,32 @@ function displayData(data) {
     sunset.textContent = `${convertUnixTime(data.sys.sunset)}`;
 }
 
+function displayData2(data){
+
+    const convertToDayOfWeek = (dt) => { const date = new Date(dt * 1000); 
+        const options = {weekday: 'long'}; 
+        return date.toLocaleDateString('en-US', options); 
+    };
+
+    // weatherForecastContainer.innerHTML = '';
+    while (weatherForecastContainer.children.length > 1) { 
+        weatherForecastContainer.removeChild(weatherForecastContainer.lastChild); 
+    }
+
+    const days = [data.list[0], data.list[1], data.list[2]]; 
+    days.forEach((day, index) => { 
+        const dayElement = document.createElement('p'); 
+        dayElement.innerHTML = `${convertToDayOfWeek(day.dt)}: ${day.main.temp.toFixed(1)} &deg;C`; 
+        weatherForecastContainer.appendChild(dayElement); });
+        
+}
+
 function convertUnixTime(unixTime) { 
     const date = new Date(unixTime * 1000); 
     const hours = date.getHours().toString().padStart(2, '0'); 
     const minutes = date.getMinutes().toString().padStart(2, '0'); 
     return `${hours}:${minutes}`; 
 }
+
+apiFetch();
+apiFetch2();
