673e699dfc4a2ba29a3351a42be730eef0ba70e8
samiclaros <cla24007@byui.edu>
2024-11-29 23:15:14 -0400

    Fixed error in html

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -21,38 +21,38 @@ function formatTimestamp(timestamp) {
 }
 
 const showInfo = document.querySelector("#results");
-    showInfo.innerHTML = `
-        <h2>Thank you for your Application!</h2>
-        <table>
-            <tr>
-                <td>Aplication For:</td>
-                <td>${show('firstName')} ${show('lastName')}</td>
-            </tr>
-            <tr>
-                <td>Organizational Title:</td>
-                <td>${show('orgTitle')}</td>
-            </tr>
-            <tr>
-                <td>Email Address:</td>
-                <td>${show('email')}</td>
-            </tr>
-            <tr>
-                <td>Mobile Phone Number:</td>
-                <td>${show('phone')}</td>
-            </tr>
-            <tr>
-                <td>Company/Organization Name:</td>
-                <td>${show('organization')}</td>
-            </tr>
-            <tr>
-                <td>Membership Level:</td>
-                <td>${show('membershipLevel')}</td>
-            </tr>
-            <tr>
-                <td>Date & Time:</td>
-                <td>${formatTimestamp()}</td>
-            </tr>
-        </table>
-    `;
+
+showInfo.innerHTML = `
+    <h2>Thank you for your Application!</h2>
+    <table>
+        <tr>
+            <td>Aplication For:</td>
+            <td>${show('firstName')} ${show('lastName')}</td>
+        </tr>
+        <tr>
+            <td>Organizational Title:</td>
+            <td>${show('orgTitle')}</td>
+        </tr>
+        <tr>
+            <td>Email Address:</td>
+            <td>${show('email')}</td>
+        </tr>
+        <tr>
+            <td>Mobile Phone Number:</td>
+            <td>${show('phone')}</td>
+        </tr>
+        <tr>
+            <td>Company/Organization Name:</td>
+            <td>${show('organization')}</td>
+        </tr>
+        <tr>
+            <td>Membership Level:</td>
+            <td>${show('membershipLevel')}</td>
+        </tr>
+        <tr>
+            <td>Date & Time:</td>
+            <td>${formatTimestamp()}</td>
+        </tr>
+    </table>`;
 
 
