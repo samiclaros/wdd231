16fe05f164151f3dbcb4f7b1b3c1f324c11e4525
samiclaros <cla24007@byui.edu>
2024-12-04 22:22:25 -0400

    Trying to add a calendar to the discover page

---
+++ chamber/scripts/calendar.js
@@ -0,0 +1,30 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+        
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//         initialView: 'dayGridMonth',
+//         headerToolbar: {
+//             left: 'prev,next today',
+//             center: 'title',
+//             right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//         },
+//         events: [
+                            
+//                 {
+//                     title: 'Evento 1',
+//                     start: '2024-06-01'
+//                 },
+//                 {
+//                     title: 'Evento 2',
+//                     start: '2024-06-07',
+//                     end: '2024-06-10'
+//                 },
+//                 {
+//                     title: 'Evento 3',
+//                     start: '2024-06-09T16:00:00'
+//                 }
+//         ]
+//         });
+        
+//         calendar.render();
+// });
