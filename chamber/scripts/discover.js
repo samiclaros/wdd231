49b6804daaf56b07fbff5ad5b0d3cd01c3ce1320
samiclaros <cla24007@byui.edu>
2024-12-06 16:09:23 -0400

    Local storage done

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -97,4 +97,25 @@ renderAttractions();
 
 // Attraction cards
 
-// VISITS or LocalStorage
+// Datetimenow
+const currentDateLocalStorage = new Date();
+const msToDays = 86400000;
+
+
+const lastVisit = localStorage.getItem("lastVisit");
+const visitMessageElement = document.querySelector("#visits");
+
+if(!lastVisit) {
+  visitMessageElement.textContent = "Welcome! Let us know if you have any questions.";
+} else{
+  const lastVisitDate = new Date(lastVisit);
+  const daysSinceLastVisit = Math.round((currentDateLocalStorage - lastVisitDate) / msToDays);
+  
+  if(daysSinceLastVisit < 1) {
+    visitMessageElement.textContent = "Back so soon! Awesome!";
+  } else{
+    visitMessageElement.textContent = `You last visited was ${daysSinceLastVisit} days ago.`;
+  }
+}
+
+localStorage.setItem('lastVisit', currentDateLocalStorage.toISOString());
