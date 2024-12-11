aca7f5652186bff6a057c64986e470b1e02ad1bc
samiclaros <cla24007@byui.edu>
2024-12-11 17:26:29 -0400

    Mobile view done

---
+++ project/data/home.mjs
@@ -0,0 +1,26 @@
+export const info = [
+    {
+      "name": "Draks Belt (Discount)",
+      "image": "./images/topBelt.webp",
+      "model": "./images/topBeltModel.webp",
+      "discount": "20% Discount!",
+      "price": "$20.00",
+      "description": "A stylish and durable belt perfect for any occasion. Made from high-quality materials, this belt offers both comfort and elegance.",
+    },
+    {
+      "name": "Moroni Pants (Discount)",
+      "image": "./images/topPants.webp",
+      "model": "./images/topPantsModel.webp",
+      "discount": "15% discount!",
+      "price": "$40.00",
+      "description": "Comfortable and versatile pants suitable for both casual and formal wear. These pants are designed to provide a perfect fit and long-lasting comfort.",
+    },
+    {
+      "name": "Alma Shoes (Discount)",
+      "image": "./images/topShoe.webp",
+      "model": "./images/topShoeModel.webp",
+      "discount": "30% discount!",
+      "price": "$50.00",
+      "description": "High-quality shoes that combine style and comfort. Ideal for everyday use, these shoes are crafted to provide excellent support and durability.",
+    }
+];
---
+++ project/scripts/home.js
@@ -0,0 +1,82 @@
+import { info } from '../data/home.mjs';
+
+function displayblackFriday() {
+    const blackFridayContainer = document.getElementById('blackFriday');
+    blackFridayContainer.innerHTML = '';
+
+    const h2 = document.createElement('h2'); 
+    h2.textContent = 'TOP DISCOUNTS!'; 
+    blackFridayContainer.appendChild(h2);
+
+    info.forEach(info => {
+        const card = document.createElement('div');
+        const img = document.createElement('img');
+        const discount = document.createElement('p');
+    
+        img.setAttribute('src', info.image);
+        img.setAttribute('alt', info.name);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute("width", "200");
+        img.setAttribute("height", "auto");
+        h2.textContent = `TOP DISCOUNTS!`;
+        discount.textContent = info.discount;
+    
+        card.appendChild(img);
+        card.appendChild(discount);
+        blackFridayContainer.appendChild(card);
+    })
+  }
+  
+displayblackFriday();
+
+function displayModel() {
+    const modelContainer = document.getElementById('model');
+    modelContainer.innerHTML = '';
+
+    const h2 = document.createElement('h2'); 
+    h2.textContent = 'MODELS'; 
+    modelContainer.appendChild(h2);
+
+    info.forEach(info => {
+        const card = document.createElement('section');
+        const img = document.createElement('img');
+        const button = document.createElement('button');
+    
+        img.setAttribute('src', info.model);
+        img.setAttribute('alt', info.name);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute("width", "200");
+        img.setAttribute("height", "auto");
+
+        button.textContent = "View Details";
+        button.addEventListener('click', () => {
+            displaySalesDetails(info);
+        });
+    
+        card.appendChild(img);
+        card.appendChild(button);
+        modelContainer.appendChild(card);
+    })
+}
+
+displayModel()
+
+const salesDetails = document.querySelector("#salesDetails");
+
+function displaySalesDetails(info) {
+    salesDetails.innerHTML = `
+      <button id="closeModal">❌</button>
+      <h2>Name: ${info.name}</h2>
+      <h3>Discount: ${info.discount}</h3>
+      <p><strong>Price</strong>: ${info.price}</p>
+      <p>Description: ${info.description}</p>
+    `;
+
+    salesDetails.showModal();
+  
+    const closeModal = document.querySelector("#closeModal");
+    closeModal.addEventListener("click", () => {
+      salesDetails.close();
+    });
+}
+
