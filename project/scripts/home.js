9fffb0bec862a5d046d0d0c3a0b5f1a83bc422ca
samiclaros <cla24007@byui.edu>
2024-12-12 12:40:37 -0400

    Index finished

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -80,3 +80,20 @@ function displaySalesDetails(info) {
     });
 }
 
+
+// LAST MODIFIED
+const lastModified = document.querySelector("#lastModified");
+const lastModifiedDate = new Date(document.lastModified);
+
+lastModified.innerHTML = `Last modified: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+	dateStyle: "short",
+    timeStyle: "medium"
+	}
+).format(lastModifiedDate)}</span>`;
+
+// CURRENT YEAR
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()} </span>`;
